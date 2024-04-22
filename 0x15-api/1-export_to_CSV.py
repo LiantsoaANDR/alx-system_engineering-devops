e12bb979efad14f95b29990058f50471d3d713fb
LiantsoaANDR <liantsoa.cpro@gmail.com>
2024-04-22 11:47:45 -0700

    Task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""From task 0,  Python script to export data in the CSV format"""
+
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    Id = int(sys.argv[1])
+    user = requests.get('https://jsonplaceholder.typicode.com/users')
+    user_data = user.json()
+    for i in user_data:
+        if i.get('id') == Id:
+            name = i.get('name')
+            break
+
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos/')
+    todo_data = todos.json()
+    file_name = "{}.csv".format(Id)
+
+    with open(file_name, mode='w', newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',', quotechar='"',
+                            quoting=csv.QUOTE_ALL, lineterminator='\n')
+        for data in todo_data:
+            if data.get('userId') == Id:
+                task_status = 'True' if data.get('completed') else 'False'
+                task_title = data.get('title')
+                writer.writerow([Id, name, task_status, task_title])
